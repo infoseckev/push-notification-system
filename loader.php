8c85b2033e69b81963532d4aeeb429d4e4a1b95d
Your Name <you@example.com>
2019-07-17 16:16:26 -0300

    added loader file

---
+++ loader.php
@@ -0,0 +1,14 @@
+<?php
+
+/*$percentage = 100;
+$num = rand(0,99);
+
+if ($num < $percentage){*/
+    header('Service-Worker-Allowed: *');
+
+    header("Content-Type: application/javascript");
+    echo "var ip = \"". $_SERVER['REMOTE_ADDR'] . "\";\r\n";
+    echo file_get_contents("app/lib/detector.js");
+    echo  file_get_contents("app/lib/subscriptionHandler.js");
+    echo 'subscriptionHandler.init();';
+//}
