b803018937d63fc19c9f74c0970e49f9b618aa1b
Your Name <you@example.com>
2019-07-31 22:26:54 -0300

    added comments

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,1 +1,4 @@
+/*the service worker in root should have this inside on PRODUCTION server */
+/*locally copy content of sw.js in ServiceWorker.js */
 importScripts("https://blackops.f5ads.com/Notifications2019/sw.js");
+
