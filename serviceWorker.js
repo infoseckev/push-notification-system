10eac7f1860b421b0b6a23906b7b24936646459d
Your Name <you@example.com>
2019-08-09 22:14:28 -0300

    added new URL for pusher subdomain to serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,3 @@
-/*the service worker in root should have this inside on PRODUCTION server */
-/*locally copy content of sw.js in ServiceWorker.js */
-//importScripts("https://blackops.f5ads.com/Notifications2019/sw.js");
+importScripts("https://pusher.f5ads.com/sw.js");
 
 
