ff44d9c4faf5c171c72384c293c09adb1ea0bfa4
Your Name <you@example.com>
2019-08-09 23:42:20 -0300

    F5AD-47 #comment fixed paths and scheduled job

--- app/keys/cronjob.php
+++ app/keys/cronjob.php
@@ -1,17 +1,14 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-require __DIR__ . '/vendor/autoload.php';
+
+require '/var/www/pusher.f5ads.com/public_html/app/vendor/autoload.php';
 use SecureEnvPHP\SecureEnvPHP;
-(new SecureEnvPHP())->parse('../.env.enc', 'keys/.env.key');
-include 'classes/db.php';
-include 'classes/SendNotification.php';
+(new SecureEnvPHP())->parse('/var/www/pusher.f5ads.com/public_html/.env.enc', '/var/www/pusher.f5ads.com/public_html/app/keys/.env.key');
+include '/var/www/pusher.f5ads.com/public_html/app/classes/db.php';
+include '/var/www/pusher.f5ads.com/public_html/app/classes/SendNotification.php';
 
 $dbhost = getenv('DB_HOST');
 $dbuser = getenv('DB_USER');
 $dbpass = getenv('DB_PASS');
-
 $dbname = getenv('DB_NAME');
 $db = new db($dbhost, $dbuser, $dbpass, $dbname);
 
@@ -19,9 +16,7 @@ $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 $results = $db->query('SELECT id, endpointId, dateToSend, click_url, title, icon_url, image_url, message, public_key, auth_token, sent_id, domain_id
                     FROM notifications_queue WHERE dateToSend <= NOW()')->fetchAll();
 
-//you left off troubleshooting this
 foreach($results as $res){
-
 
     SendNotification::sendNotification($res['endpointId'], $res['public_key'], $res['auth_token'],  $res['message'], $res['title'],  $res['icon_url'],  $res['image_url'],  $res['click_url'], $res['sent_id'],$res['domain_id'], $db, $res['id']);
     $stmt = $conn->prepare("DELETE FROM notifications_queue WHERE `id` = ?");
