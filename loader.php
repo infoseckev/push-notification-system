a0d9f566994127b325b748e8527b7e4eefbe2506
Your Name <you@example.com>
2019-08-10 15:54:51 -0300

    F5AD-59 #comment added database call for each page load

--- loader.php
+++ loader.php
@@ -1,15 +1,42 @@
 <?php
+
+require __DIR__ . '/app/vendor/autoload.php';
+use SecureEnvPHP\SecureEnvPHP;
+(new SecureEnvPHP())->parse('.env.enc', 'app/keys/.env.key');
+
 $tracking_gps =  ($_GET['GPS']);
-/*$percentage = 100;
-$num = rand(0,99);
 
-if ($num < $percentage){*/
-    header('Service-Worker-Allowed: *');
+header('Service-Worker-Allowed: *');
+header("Content-Type: application/javascript");
 
-    header("Content-Type: application/javascript");
-    echo "var ip = \"". $_SERVER['REMOTE_ADDR'] . "\";\r\n";
-    echo "var gps = \"". $tracking_gps . "\";\r\n";
-    echo file_get_contents("app/lib/detector.js");
-    echo  file_get_contents("app/lib/subscriptionHandler.js");
-    echo 'subscriptionHandler.init();';
+//init javascript for subscription
+echo "var ip = \"". $_SERVER['REMOTE_ADDR'] . "\";\r\n";
+echo "var gps = \"". $tracking_gps . "\";\r\n";
+echo file_get_contents("app/lib/detector.js");
+echo  file_get_contents("app/lib/subscriptionHandler.js");
+echo 'subscriptionHandler.init();';
+
+//insert load in db
+$ip = '';
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+$domain_url = $_SERVER['HTTP_HOST'];
+$dbhost = getenv('DB_HOST');
+$dbuser = getenv('DB_USER');
+$dbpass = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+$stmt = $conn->prepare("INSERT INTO `moon`.`loader_stats`
+(`domain_url`,
+`ip`)
+VALUES (?, ?)");
+
+$stmt->bind_param("ss",$domain_url, $ip);
+$stmt->execute();
 //}
