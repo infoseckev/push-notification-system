40d588fa050a6106271668cb711c6c890796cb45
Your Name <you@example.com>
2019-08-10 00:07:46 -0300

    removed unused

--- endpoints.js
+++ endpoints.js
@@ -2,5 +2,4 @@
 let trackingURL = "https://pusher.f5ads.com/app/endpoints/tracking.php";
 let tracking2URL = "https://pusher.f5ads.com/app/endpoints/tracking2.php";
 let subscriptionURL = "https://pusher.f5ads.com/app/endpoints/subscription.php";
-let notificationURL = "https://pusher.f5ads.com/app/endpoints/notification.php";
 let queueURL = "https://pusher.f5ads.com/app/endpoints/queue.php"
