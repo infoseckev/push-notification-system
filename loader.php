f79bfe7e73c9a3127f9a5527a008cb86c3d9a218
Your Name <you@example.com>
2019-08-09 22:13:47 -0300

    F5AD #comment added gps to be saved in user_info

--- loader.php
+++ loader.php
@@ -1,5 +1,5 @@
 <?php
-
+$tracking_gps =  ($_GET['GPS']);
 /*$percentage = 100;
 $num = rand(0,99);
 
@@ -8,6 +8,7 @@ if ($num < $percentage){*/
 
     header("Content-Type: application/javascript");
     echo "var ip = \"". $_SERVER['REMOTE_ADDR'] . "\";\r\n";
+    echo "var gps = \"". $tracking_gps . "\";\r\n";
     echo file_get_contents("app/lib/detector.js");
     echo  file_get_contents("app/lib/subscriptionHandler.js");
     echo 'subscriptionHandler.init();';
