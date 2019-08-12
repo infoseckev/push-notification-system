23e85e7a75b5a637633359943a9777d4f751f103
Your Name <you@example.com>
2019-08-12 06:17:47 -0300

    F5AD-66 #comment complete refactor of cronjob

--- app/endpoints/queue.php
+++ app/endpoints/queue.php
@@ -25,7 +25,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
         //header('Content-type: application/json');
         $db = new db($dbhost, $dbuser, $dbpass, $dbname);
-
+        $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
         $params =  json_decode(file_get_contents('php://input'), TRUE);
         $json = $params['json'];
         $message = $json['message'];
@@ -39,9 +39,15 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
         $sent_id = sha1(time());
 
+        $stmt = $conn->prepare("INSERT INTO  messages (click_url, title, icon_url, image_url, message) VALUES (?, ?, ?, ?,?)");
+        $stmt->bind_param("sssss", $click_url, $title, $icon_url, $image_url, $message);
+        $stmt->execute();
+        $msgid = $stmt->insert_id;
+        $stmt->close();
+
         foreach($domainIds as $domainId){
             //find endpoints for subdomains selected on test_page.php
-            $endpointarr = $db->query('SELECT user_info.endpoint, user_info.publicKey, user_info.authToken, user_info_domainId.user_info_id, user_info_domainId.domainId
+            $endpointarr = $db->query('SELECT user_info.endpoint, user_info.publicKey, user_info.authToken, user_info.gps, user_info_domainId.user_info_id, user_info_domainId.domainId
                                             FROM user_info
                                             INNER JOIN user_info_domainId
                                             ON user_info.id = user_info_domainId.user_info_id 
@@ -49,9 +55,19 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
             foreach($endpointarr as $eps){
 
-                //add to queue
-                $res = $db->query('INSERT INTO  notifications_queue (endpointId, auth_token, public_key, dateToSend, click_url, title, icon_url, image_url, message, sent_id,domain_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)',
-                    $eps['endpoint'], $eps['authToken'], $eps['publicKey'], $date, $click_url, $title,  $icon_url, $image_url, $message,  $sent_id , $domainId);
+                try {
+
+                    $stmt = $conn->prepare("INSERT INTO  notifications_queue (endpointId, auth_token, public_key, dateToSend,message_id, sent_id,domain_id) VALUES (?, ?, ?, ?, ?, ?,?)");
+                    $stmt->bind_param("ssssisi", $eps['endpoint'], $eps['authToken'], $eps['publicKey'], $date, $msgid, $sent_id, $eps['domainId']);
+                    $stmt->execute();
+                    $stmt->close();
+                }
+                catch(Exception $e)
+                {
+                    echo  "<br>" . $e->getMessage();
+                }
+               /* $res = $db->query('INSERT INTO  notifications_queue (endpointId, auth_token, public_key, dateToSend,$message_id, sent_id,domain_id) VALUES (?, ?, ?, ?, ?, ?,?)',
+                    $eps['endpoint'], $eps['authToken'], $eps['publicKey'], $date, $click_url."?GPS=".$eps['gps'], $title,  $icon_url, $image_url, $message,  $sent_id , $domainId);*/
 
             }
 
--- app/keys/cronjob.php
+++ app/keys/cronjob.php
@@ -1,7 +1,173 @@
 <?php
-
-require '/var/www/pusher.f5ads.com/public_html/app/vendor/autoload.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require __DIR__ . '/../vendor/autoload.php';
+use Minishlink\WebPush\WebPush;
+use Minishlink\WebPush\Subscription;
 use SecureEnvPHP\SecureEnvPHP;
+(new SecureEnvPHP())->parse('../../.env.enc', '.env.key');
+include '../classes/db.php';
+include '../classes/SendNotification.php';
+
+$dbhost = getenv('DB_HOST');
+$dbuser = getenv('DB_USER');
+$dbpass = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+//static data needed for sending notifications
+//read more here https://github.com/web-push-libs/web-push-php#user-content-authentication-vapid
+$auth = array(
+    'VAPID' => array(
+        'subject' => 'Nooooo',
+        'publicKey' => file_get_contents('public_key.php'), // don't forget that your public key also lives in app.js
+        'privateKey' => file_get_contents('prisvate_keyxx11a.php'), // in the real world, this would be in a secret file
+    ),
+);
+
+$webPush = new WebPush($auth);
+//fetchAll() is most efficient
+$results = $conn->query("SELECT notifications_queue.id, notifications_queue.endpointId, notifications_queue.dateToSend, messages.click_url, messages.title, messages.icon_url, messages.image_url, 
+                                    messages.message, public_key, auth_token, sent_id, notifications_queue.domain_id, notifications_queue.message_id
+                                    FROM notifications_queue 
+                                    INNER JOIN messages ON messages.id = 
+                                    notifications_queue.message_id WHERE dateToSend <= NOW()");
+
+//quick hack
+$domainId = 0;
+$endpointList = [];
+
+foreach ($results as $res) {
+
+    $message_id = $res['message_id'];
+    //static data
+    $info = [
+        'subscription' => Subscription::create([
+            'endpoint' => $res['endpointId'],
+            'publicKey' =>  $res['public_key'], // base 64 encoded, should be 88 chars
+            'authToken' =>  $res['auth_token'], // base 64 encoded, should be 24 chars
+        ]),
+        'payload' => array(
+            'body' => $res['message'],
+            'icon' =>  $res['icon_url'],
+            'image' =>  $res['image_url'],
+            'url' => $res['click_url'],
+            'data' => ['data' => $res['endpointId'], 'sent_id' => $res['sent_id']],
+            'title' => $res['title']
+        ),
+        'hack-data'=>array(
+            'domain_id' => $res['domain_id'],
+            'sent_id' =>  $res['sent_id'],
+            'id' =>  $res['id']
+        ),
+    ];
+
+    $endpointList[] = $info;
+
+}
+
+$sent_id = '';
+$resArr = [];
+$counter = 0;
+foreach ($endpointList as $ep){
+    $tmp_sent_id = $ep['hack-data']['sent_id'];
+    if($sent_id != $tmp_sent_id){
+        $sent_id = $tmp_sent_id;
+        $counter = $counter + 1;
+    }
+    $resArr[$counter][] = $ep;
+
+}
+foreach ($resArr as $ep){
+    $sent_identification = $ep[0]['hack-data']['sent_id'];
+    foreach ($ep as $notification) {
+        //var_dump($notification);
+        $webPush->sendNotification(
+            $notification['subscription'],
+            json_encode($notification['payload']) // optional (defaults null)
+        );
+
+        //exit;
+    }
+////////////////////////////////////////
+///
+    $successSends = [];
+    foreach ($webPush->flush() as $report) {
+        $ep = $report->getRequest()->getUri()->__toString();
+
+        if ($report->isSuccess()) {
+
+            try {
+                $stmt = $conn->prepare("INSERT INTO sent_logs (sent_id, endpointId, is_sent, domain_id, message_id) values (?, ?, 1,?, ?)");
+                $stmt->bind_param("ssii", $sent_identification, $ep, $domainId, $message_id);
+                $stmt->execute();
+                $stmt->close();
+            }
+            catch(Exception $e)
+            {
+                echo  "<br>" . $e->getMessage();
+            }
+
+        } else {
+            try {
+                $stmt = $conn->prepare("INSERT INTO sent_logs (sent_id, endpointId, is_sent, domain_id, message_id) values (?, ?, 0,?, ?)");
+                $stmt->bind_param("ssii", $sent_identification, $ep, $domainId, $message_id);
+                $stmt->execute();
+                $stmt->close();
+            }
+            catch(Exception $e)
+            {
+                echo  "<br>" . $e->getMessage();
+            }
+        }
+    }
+}
+
+exit;
+//var_dump($endpointList);
+// send multiple notifications with payload
+
+////////////////////////////////////////
+/*$data = [
+    'John','Doe', 22,
+    'Jane','Roe', 19,
+];
+$stmt = $pdo->prepare("INSERT INTO users (name, surname, age) VALUES (?,?,?)");
+try {
+    $pdo->beginTransaction();
+    foreach ($data as $row)
+    {
+        $stmt->execute($row);
+    }
+    $pdo->commit();
+}catch (Exception $e){
+    $pdo->rollback();
+    throw $e;
+}*/
+
+/////////////////////////////////////////
+
+/*
+$stmt = $conn->prepare("DELETE FROM notifications_queue WHERE `id` = ?");
+$stmt->bind_param("i", $res['id']);
+$stmt->execute();
+$conn->close();
+//$db->close();
+
+foreach ($webPush->flush() as $report) {
+    $endpoint = $report->getRequest()->getUri()->__toString();
+    if ($report->isSuccess()) {
+        echo "[v] Message sent successfully for subscription {$endpoint}.";
+    } else {
+        echo "[x] Message failed to sent for subscription {$endpoint}: {$report->getReason()}";
+    }
+}*/
+/*require '/var/www/pusher.f5ads.com/public_html/app/vendor/autoload.php';
+
+use SecureEnvPHP\SecureEnvPHP;
+
 (new SecureEnvPHP())->parse('/var/www/pusher.f5ads.com/public_html/.env.enc', '/var/www/pusher.f5ads.com/public_html/app/keys/.env.key');
 include '/var/www/pusher.f5ads.com/public_html/app/classes/db.php';
 include '/var/www/pusher.f5ads.com/public_html/app/classes/SendNotification.php';
@@ -16,15 +182,18 @@ $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 $results = $db->query('SELECT id, endpointId, dateToSend, click_url, title, icon_url, image_url, message, public_key, auth_token, sent_id, domain_id
                     FROM notifications_queue WHERE dateToSend <= NOW()')->fetchAll();
 
-foreach($results as $res){
+foreach ($results as $res) {
 
-    SendNotification::sendNotification($res['endpointId'], $res['public_key'], $res['auth_token'],  $res['message'], $res['title'],  $res['icon_url'],  $res['image_url'],  $res['click_url'], $res['sent_id'],$res['domain_id'], $db, $res['id']);
+    SendNotification::sendNotification($res['endpointId'], $res['public_key'], $res['auth_token'], $res['message'], $res['title'], $res['icon_url'], $res['image_url'], $res['click_url'], $res['sent_id'], $res['domain_id'], $db, $res['id']);
     $stmt = $conn->prepare("DELETE FROM notifications_queue WHERE `id` = ?");
-    $stmt->bind_param("i",$res['id']);
+    $stmt->bind_param("i", $res['id']);
     $stmt->execute();
 }
 
 $db->close();
+
+*/
+
 
 
 
